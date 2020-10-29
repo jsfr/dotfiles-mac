ac15be4b5b74b4b35680966a2db7c3394f78d8c5
Jens Fredskov <jensfredskov@protonmail.ch>
2020-10-29 14:41:42 +0100

    YOLO: Version control is awful

--- .hammerspoon/bitday.lua
+++ .hammerspoon/bitday.lua
@@ -1,4 +1,12 @@
 -- luacheck: globals hs
+--
+local function getWallpaper()
+    local connection = hs.sqlite3.open(os.getenv("HOME") .. "/Library/Application Support/Dock/desktoppicture.db")
+    for a in connection:rows("select value from data limit 1") do
+        connection:close()
+        return a[1];
+    end
+end
 
 local function setWallpaper(file)
     local connection = hs.sqlite3.open(os.getenv("HOME") .. "/Library/Application Support/Dock/desktoppicture.db")
@@ -8,16 +16,18 @@ local function setWallpaper(file)
     dock:kill()
 end
 
-local function getWallpaper(wallpaperName)
-     return hs.fs.pathToAbsolute("./bitday/" .. wallpaperName .. ".png")
-end
-
 local function setNextWallpaper()
     local date = os.date("*t")
-    local hour = math.floor(date.hour/2)
-    local wallpaperName = string.format("%02d", (hour % 12) + 1)
-    local wallpaper = getWallpaper(wallpaperName)
-    setWallpaper(wallpaper)
+    local number = (((math.floor(date.hour/2) % 12) + 10) % 12) + 1
+
+    local wallpaperName = string.format("%02d", number)
+
+    local nextWallpaper = hs.fs.pathToAbsolute("./bitday/" .. wallpaperName .. ".png")
+    local currentWallpaper = getWallpaper()
+
+    if (nextWallpaper ~= currentWallpaper) then
+        setWallpaper(nextWallpaper)
+    end
 end
 
 local function updateOnWake(event)
