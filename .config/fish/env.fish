c3565dd62d790505bf8ac36beaf853c9ff6fd849
Jens Fredskov <jsfr@users.noreply.github.com>
2020-11-12 15:43:19 +0100

    YOLO: My boss forced me to build this feature... Pure shit.

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,50 +1,52 @@
+if test -n "$ENV_EXPORTED"
+    # Environment is already exported
+    exit 0
+end
+
 set -x PAGER less
 set -x FZF_DEFAULT_COMMAND "fd -It f"
 set -x EDITOR "nvim"
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 set -x LANG "en_US.UTF-8"
-set -x LC_CTYPE "en_US.UTF-8"
-set -x LC_NUMERIC "en_US.UTF-8"
-set -x LC_TIME "en_US.UTF-8"
-set -x LC_COLLATE "en_US.UTF-8"
-set -x LC_MONETARY "en_US.UTF-8"
-set -x LC_MESSAGES "en_US.UTF-8"
-set -x LC_PAPER "en_US.UTF-8"
-set -x LC_NAME "en_US.UTF-8"
-set -x LC_ADDRESS "en_US.UTF-8"
-set -x LC_TELEPHONE "en_US.UTF-8"
-set -x LC_MEASUREMENT "en_US.UTF-8"
-set -x LC_IDENTIFICATION "en_US.UTF-8"
-set -x LC_ALL "en_US.UTF-8"
+set -x LC_CTYPE $LANG
+set -x LC_NUMERIC $LANG
+set -x LC_TIME $LANG
+set -x LC_COLLATE $LANG
+set -x LC_MONETARY $LANG
+set -x LC_MESSAGES $LANG
+set -x LC_PAPER $LANG
+set -x LC_NAME $LANG
+set -x LC_ADDRESS $LANG
+set -x LC_TELEPHONE $LANG
+set -x LC_MEASUREMENT $LANG
+set -x LC_IDENTIFICATION $LANG
+set -x LC_ALL $LANG
 
 set -x XDG_CONFIG_HOME "$HOME/.config"
 set -x GOPATH "$HOME/go"
 set -x JAVA_HOME (/usr/libexec/java_home -v 11)
 
-# set -x LDFLAGS "-L/usr/local/opt/node@12/lib"
-# set -x CPPFLAGS "-I/usr/local/opt/node@12/include"
-
-if test -e "$HOME/.homebrew_github_token"
-    set -x HOMEBREW_GITHUB_API_TOKEN (cat "$HOME/.homebrew_github_token")
+set -x HOMEBREW_GITHUB_API_TOKEN_PATH "$HOME/.homebrew_github_token"
+if test -e $HOMEBREW_GITHUB_API_TOKEN_PATH
+    set -x HOMEBREW_GITHUB_API_TOKEN (cat $HOMEBREW_GITHUB_API_TOKEN_PATH)
 end
 
-if test -e "$HOME/.npm_token"
-    set -x NPM_TOKEN (cat "$HOME/.npm_token")
-end
-
-
-if test -z "$PATH_EXPORTED"
-    set -x PATH $PATH \
-                "$GOPATH/bin" \
-                "$HOME/.cargo/bin" \
-                "$HOME/.local/bin" \
-                "$HOME/Repos/github.com/pleo-io/pleo/bin" \
-                "/usr/local/opt/node@12/bin" \
-                "/usr/local/sbin"
-    set -x PATH_EXPORTED "exported"
+set -x NPM_TOKEN_PATH "$HOME/.npm_token"
+if test -e $NPM_TOKEN_PATH
+    set -x NPM_TOKEN (cat $NPM_TOKEN_PATH)
 end
 
 set -x RUST_SRC_PATH (rustc --print sysroot)"/lib/rustlib/src/rust/src"
-
 set -x ANDROID_HOME "$HOME/Library/Android/sdk"
+set -x N_PREFIX "$HOME/.n"
+
+set -x PATH $PATH \
+            "$GOPATH/bin" \
+            "$HOME/.cargo/bin" \
+            "$HOME/.local/bin" \
+            "$HOME/Repos/github.com/pleo-io/pleo/bin" \
+            "/usr/local/sbin" \
+            "$N_PREFIX/bin"
+
+set -x ENV_EXPORTED "exported"
