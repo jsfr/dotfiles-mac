a0c9c270149abe48dc00b262224c8dcd5192f9d4
Jens Fredskov <jsfr@users.noreply.github.com>
2021-05-16 23:31:32 +0200

    YOLO: It works!

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,11 +1,16 @@
 -- luacheck: globals token spaces pull_requests
-require("auto_reload")
-require("hs.ipc")
 
-token = require("token")
+-- setup luarocks
+package.path = package.path .. ";" .. os.getenv("HOME") .. "/.luarocks/share/lua/5.4/?.lua;" .. os.getenv("HOME") .. "/.luarocks/share/lua/5.4/?/init.lua"
+package.cpath = package.cpath .. ";" .. os.getenv("HOME") .. "/.luarocks/lib/lua/5.4/?.so"
 
-pull_requests = require("pull_requests")
-pull_requests.update()
+-- leagcy lua configuration
+-- pull_requests = require("pull_requests")
+-- pull_requests.update()
 
-spaces = require("spaces")
-spaces.update()
+-- setup fennel
+local fennel = require("fennel")
+table.insert(package.loaders or package.searchers, fennel.searcher)
+
+-- run fennel init file
+fennel.dofile("init.fnl", { allowedGlobals = false })
