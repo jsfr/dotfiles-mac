4b202c707b679cf9f7b8481ac886bdb605983a84
Jens Fredskov <jsfr@users.noreply.github.com>
2023-08-21 15:10:55 +0200

    YOLO: I'm too old for this shit!

--- .desk/desks/kubeconfig.fish
+++ .desk/desks/kubeconfig.fish
@@ -1,5 +1,22 @@
-set -gx KUBECONFIG $HOME/.kube/config_desk
+if test -z "$DESK_EXPORTED"
+    osascript -e "
+    tell application \"System Events\" to tell application process \"NextDNS\"
+    click menu bar item 1 of menu bar 1
+    click menu item \"Disable\" of menu 1 of menu bar item 1 of menu bar 1
+    end tell
+    " > /dev/null
+    function exit_handler --on-event fish_exit
+        osascript -e "
+        tell application \"System Events\" to tell application process \"NextDNS\"
+        click menu bar item 1 of menu bar 1
+        click menu item \"Enable\" of menu 1 of menu bar item 1 of menu bar 1
+        end tell
+        " > /dev/null
+        osascript -e "quit app \"Access\"" > /dev/null
+    end
 
-echo "  ####################################################"
-echo "  ### Remember to disable NextDNS and start Access ###"
-echo "  ####################################################"
+    open -g -j -a Access
+
+    set -gx KUBECONFIG $HOME/.kube/config_desk
+    set -gx DESK_EXPORTED true
+end
