0dfe17ecfe29d9eee1397789e8cb723864511c59
Jens Fredskov <jsfr@users.noreply.github.com>
2023-09-02 11:08:13 +0200

    YOLO: For real, this time.

--- .hammerspoon/init.lua
+++
@@ -1,8 +0,0 @@
--- setup fennel
-local fennel = require("fennel")
-
--- allow requiring of fennel modules
-table.insert(package.loaders or package.searchers, fennel.searcher)
-
--- run fennel init file
-fennel.dofile("init.fnl", { allowedGlobals = false })
